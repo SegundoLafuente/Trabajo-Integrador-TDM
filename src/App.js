b02c5f3873958077259f5f037cb2000974b278bc
MaximoRestivo <rmaximo@udesa.edu.ar>
2026-04-01 11:48:49 -0300

    Cambios en la estructura de App.js  usando Componentes de Clase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import Footer from "./components/Footer/Footer";
-import React from 'react'
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Footer from "./components/Footer/Footer";
 import Home from "./screens/Home/Home";
-import Peliculas from "./screens/Peliculas/Peliculas";
+import Peliculas from './screens/Peliculas/Peliculas';
+import Series from './screens/Series/Series';
+import Favoritas from './screens/Favoritas/Favoritas';
+import Registro from './screens/Registro/Registro';
+import Login from './screens/Login/Login';
 
-function App(){
-  return(
-    <>
-      <Navbar/>
-    <Switch>
-        <Route path="/" exact={true} component={Home}/>
-        <Route path="/Peliculas" component={Peliculas}/>
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+        
+          <Route path="/" exact={true} component={Home} />
 
-    </Switch>
+          <Route path="/Peliculas" component={Peliculas} />
+          <Route path="/Series" component={Series} />
+          <Route path="/Favoritas" component={Favoritas} />
 
-   
-    <Footer/>
-   </>
-  );
+          <Route path="/registro" component={Registro} />
+          <Route path="/login" component={Login} />
+
+          {/* <Route component={NotFound} /> */}
+
+      </Switch>
+
+      <Footer />
+    </React.Fragment>
+    );
+  }
 }
 
-export default App
+export default App;
