02038d1e6275839e7379ae6051093ff67036de9b
ramallofelipe <ramallof@udesa.edu.ar>
2026-04-13 17:19:11 -0300

    corregi errores del registro

--- src/components/RegistroH/RegistroH.js
+++ src/components/RegistroH/RegistroH.js
@@ -14,15 +14,19 @@ class RegistroH extends Component{
 
   evitarSubmit(event) {
     event.preventDefault();
-    let Usuarios = localStorage.getItem(Usuarios)
-    let filtro = this.Usuarios.filter((usuario) => usuario[0]===this.mail & usuario[1]===this.contra)
-    if ( filtro.length!==0  || this.contra.length<6) {
+    let UsuariosStorage = localStorage.getItem('Usuarios');
+    let arrayUsuarios = UsuariosStorage ? JSON.parse(UsuariosStorage) : [];
+    let filtro = arrayUsuarios!==0 ? this.arrayUsuarios.filter((usuario) => usuario[0]===this.mail & usuario[1]===this.contra) : [];
+    
+      if ( filtro.length!==0  || this.contra.length<6) {
       this.setState({mensajeError: 'El email ya se encuentra registrado o la contraseña es demasiado corta (mínimo 6 caracteres).'})
     } else {
       this.usuario = [this.mail, this.contra]
-      localStorage.setItem(Usuarios, this.usuario)
+      localStorage.setItem('Usuarios', this.usuario)
       this.setState({mensajeError: ''})
     }
+    
+    
   
   }
 
