fb987430df417422358cdd777ae2fd0163e55ee5
SegundoLafuente <slafuentesegura@udesa.edu.ar>
2026-04-14 16:04:11 -0300

    parte del buscador pero no esta completo

--- src/components/FormBusqueda/Form.js
+++ src/components/FormBusqueda/Form.js
@@ -1,11 +1,27 @@
 import './styles.css'
 import React, { Component } from 'react';
 import {withRouter} from 'react-router-dom';
+import Card from '../Card/Card';
 
 class Form extends Component {
   constructor(props) {
     super(props);
-    this.state = { valor: '' }; 
+    this.state = {
+      datosIniciales: [],
+      datosFiltrados: [],
+      valor: ""
+    }
+  }
+
+  componentDidMount(){
+    fetch(`https://api.themoviedb.org/3/tv/${this.state.id}?api_key=6aad86ecf8fd94ac9b44f0afc185ea99`)
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({
+        datosIniciales:data.results,
+        datosFiltrados:data.results
+      })
+    })
   }
 
   evitarSubmit(event) {
@@ -16,20 +32,33 @@ class Form extends Component {
 
   controlarCambios(event) {
     
-    this.setState(
-      { valor: event.target.value }, () => console.log(this.state.valor) 
-    );
+    this.setState({
+      valor: event.target.value
+      } , () => this.filtrarCosas(this.state.valor))
+  }
+
+  filtrarCosas(textoAFiltrar){
+    this.setState({
+      datosFiltrados: this.state.datosIniciales.filter((cosa) => cosa.name.toLowerCase().includes(textoAFiltrar.toLowerCase()))
+    })
   }
 
   render() {
     return (
-      
+      <section className = "personajes-section">
       <form onSubmit={(event) => this.evitarSubmit(event)}>
         <label>Buscar: </label>
         
-        <input type="text" onChange={(event) => this.controlarCambios(event)} value={this.state.valor} />
+        <input type="text" onChange={(event) => this.controlarCambios(event)} />
         <button type="submit"> Buscar</button>
+        
       </form>
+      {this.state.datosIniciales.length === 0 ?
+      <h3>Cargando...</h3>:
+      this.state.datosFiltrados.map((pj, idx) => <Card data = {pj} key= {pj.name+idx}/>)
+      }
+
+      </section>
     );
   }
 }
