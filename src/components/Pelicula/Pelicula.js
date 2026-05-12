7f97d22ad51faba7f27aad29299ec7159d8be728
SegundoLafuente <slafuentesegura@udesa.edu.ar>
2026-05-12 15:46:03 -0300

    pelicula actualizado

--- src/components/Pelicula/Pelicula.js
+++ src/components/Pelicula/Pelicula.js
@@ -1,19 +1,18 @@
-import React, {Component} from "react";
+import React, {useState,useEffect} from "react";
 import { Link } from "react-router-dom";
 
 
-class Pelicula extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            descripcion: "mostrar",
-            clase: "hide",
-            detalle: "Ver más",
-            favorito: "❤️"
-        }
-    }
-    componentDidMount(){
-        let storage = localStorage.getItem('favorito')
+function Pelicula (props){
+    
+        const [descripcion,setDescripcion] = useState('mostrar')
+        const [clase,setClase] = useState('hide')
+        const [detalle,setDetalle] = useState('Ver más')
+        const [favorito,setFavorito] = useState('❤️')
+        
+    
+    useEffect(
+        () => {
+            let storage = localStorage.getItem('favorito')
         if (storage !== null){
             let favoritos = JSON.parse(storage)
             let pelicula = this.props.pelicula
@@ -25,26 +24,23 @@ class Pelicula extends Component{
                 })
             }
         }
-    }
-
-    verDescripcion(){
-        if(this.state.descripcion === "mostrar"){
-            this.setState({
-                descripcion: "no mostrar",
-                clase: "mostrar",
-                detalle: "Ver menos"
-            })
+        },[]
+    )
+    
+    function verDescripcion(){
+        if(descripcion === "mostrar"){
+            setDescripcion('no mostrar')
+            setClase('mostrar')
+            setDetalle('Ver menos')
         }
         else{
-            this.setState({
-                descripcion: "mostrar",
-                clase: "hide",
-                detalle: "Ver más"
-            })
+            setDescripcion('mostrar')
+            setClase('hide')
+            setDetalle('Ver más')
         }
     }
 
-    favoritos(){
+    function favoritos(){
         let favoritos = []
         let storage = localStorage.getItem('favorito')
 
@@ -52,49 +48,51 @@ class Pelicula extends Component{
             favoritos = JSON.parse(storage)
         }
 
-        let pelicula = this.props.pelicula
+        let pelicula = props.pelicula
 
         let filtro = favoritos.filter(peli => pelicula.id === peli.id)
 
         if(filtro.length === 0){
             favoritos.push(pelicula);
             localStorage.setItem("favorito", JSON.stringify(favoritos));
-            this.setState({
-                    favorito: 'Eliminar favorito'
-                })
+    
+            setFavorito('Eliminar favorito')
         }
     }
-    eliminarfavoritos(){
-            if (this.state.favorito === 'Eliminar favorito'){
+
+    function eliminarfavoritos(){
+            if (favorito === 'Eliminar favorito'){
             let favoritos = []
             let storage = localStorage.getItem('favorito')
 
             if(storage !== null){
                 favoritos = JSON.parse(storage)
             }
-            let pelicula = this.props.pelicula
+            let pelicula = props.pelicula
 
             let nuevofav = favoritos.filter(peli => pelicula.id !== peli.id)
             localStorage.setItem('favorito', JSON.stringify(nuevofav))
-            this.setState({
-                favorito: '❤️'
-            })
+           
+            setFavorito('❤️')
         }
     }
-    render(){
+    
         return(
             <article className='single-card-movie'>
-                <img className="card-img-top" src= {`https://image.tmdb.org/t/p/w342/${this.props.pelicula.poster_path}`} alt='a'/>
-                <h2 className ="card-title">{this.props.pelicula.title}</h2>
-                <p className={this.state.clase}>{this.props.pelicula.overview}</p>
+                <img className="card-img-top" src= {`https://image.tmdb.org/t/p/w342/${props.pelicula.poster_path}`} alt='a'/>
+                <h2 className ="card-title">{props.pelicula.title}</h2>
+                <p className={clase}>{props.pelicula.overview}</p>
                 <div className="boton">
-                    <button className="btn btn-primary" onClick={()=>this.verDescripcion()}>{this.state.detalle}</button>
-                    <Link to={`/dPelicula/${this.props.pelicula.id}`}>Ir a detalle</Link>
-                    <button onClick={this.state.favorito === '❤️' ? () => this.favoritos() : () => this.eliminarfavoritos()} className ="btn alert-primary">{this.state.favorito}</button>
+                    <button className="btn btn-primary" onClick={()=>verDescripcion()}>{detalle}</button>
+                    <Link to={`/dPelicula/${props.pelicula.id}`}>Ir a detalle</Link>
+                    <button onClick={favorito === '❤️' ? () => favoritos() : () => eliminarfavoritos()} className ="btn alert-primary">{favorito}</button>
                 </div>
             </article>
         )
+
     }
-}
+    
+    
+
 
 export default Pelicula
